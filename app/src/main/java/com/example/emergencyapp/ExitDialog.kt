c5018df61c052290dd9ef1e1e12ad59d8695cafb
Mustafa_Essam <mustafaessam2004@gmail.com>
2026-03-04 20:15:33 +0200

    Update MainActivity with emergency contacts

---
+++ app/src/main/java/com/example/emergencyapp/ExitDialog.kt
@@ -0,0 +1,25 @@
+package com.example.emergencyapp
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class ExitDialog: DialogFragment() {
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+
+        builder
+            .setIcon(android.R.drawable.ic_menu_close_clear_cancel)
+            .setTitle("Exit")
+            .setMessage("Do you wanaa Exit")
+            .setPositiveButton("OK") { _, _ -> activity?.finish() }
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }
+
+        return builder.create()
+
+    }
+}
--- app/src/main/java/com/example/emergencyapp/MainActivity.kt
+++ app/src/main/java/com/example/emergencyapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +25,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val emergencies = arrayListOf<Emergency>()
+        emergencies.add(Emergency(R.drawable.police, "Police", "122"))
+        emergencies.add(Emergency(R.drawable.child, "Child Rescue", "16000"))
+        emergencies.add(Emergency(R.drawable.fire, "Fire", "180"))
+        emergencies.add(Emergency(R.drawable.hospital, "Hospital", "123"))
 
+        val adapter = EmergencyAdapter(this, emergencies)
+
+        val rv = findViewById<RecyclerView>(R.id.rv)
+        rv.adapter = adapter
+
+    }
+
+    override fun onBackPressed() {
+        val exit = ExitDialog()
+        exit.isCancelable = false
+        exit.show(supportFragmentManager, null)
     }
 }
